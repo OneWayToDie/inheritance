62b99228b345ed310057df8a4744451c117f6135
Danila <Danila@DESKTOP-44LP0PV>
2025-07-22 19:11:04 +0300

    Ценок - конец

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -3,8 +3,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-#define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
-#define HUMAN_GIVE_PARAMETERS last_name, first_name, age
+#define HUMAN_TAKE_PARAMETERES const std::string& last_name, const std::string& first_name, int age
+#define HUMAN_GIVE_PARAMETERES last_name, first_name, age
 
 class Human
 {
@@ -37,8 +37,8 @@ public:
 		this->age = age;
 	}
 
-	//					/Constructors:
-	Human(HUMAN_TAKE_PARAMETERS)
+	//					Constructors:
+	Human(HUMAN_TAKE_PARAMETERES)
 	{
 		set_last_name(last_name);
 		set_first_name(first_name);
@@ -53,11 +53,11 @@ public:
 	//					Methods:
 	void info()const
 	{
-		cout << last_name << " " << first_name << " " << age << endl;
+		cout << "Фамилия - " << last_name << ", " << "Имя - " << first_name << ", " << "Возраст - " << age << "  ";
 	}
 };
 
-#define STUDENT_TAKE_PARAMETERES const const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_TAKE_PARAMETERES  const std::string& speciality, const std::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERES  speciality, group, rating, attendance
 
 class Student:public Human
@@ -101,7 +101,7 @@ public:
 	}
 
 	//					Constructors:
-	Student(HUMAN_TAKE_PARAMETERS,STUDENT_TAKE_PARAMETERES):Human(HUMAN_GIVE_PARAMETERS)
+	Student(HUMAN_TAKE_PARAMETERES,STUDENT_TAKE_PARAMETERES):Human(HUMAN_GIVE_PARAMETERES)
 	{
 		set_speciality(speciality);
 		set_group(group);
@@ -148,7 +148,7 @@ public:
 	}
 
 	//				Constructors:
-	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERES) :Human(HUMAN_GIVE_PARAMETERS)
+	Teacher(HUMAN_TAKE_PARAMETERES, TEACHER_TAKE_PARAMETERES) :Human(HUMAN_GIVE_PARAMETERES)
 	{
 		set_speciality(speciality);
 		set_experience(experience);
@@ -165,9 +165,69 @@ public:
 	}
 };
 
-class Graduate
-{
 
+#define GRADUATE_TAKE_PARAMETERES const std::string& theme_of_work, const std::string& date_of_diploma_defense, int scores_of_practice, int scores_of_theory
+#define GRADUATE_GIVE_PARAMETEES theme_of_work, date_of_diploma_defense, scores_of_practice, scores_of_theory
+
+class Graduate :public Human
+{
+	std::string theme_of_work;
+	std::string date_of_diploma_defense;
+	int scores_of_practice;
+	int scores_of_theory;
+public:
+	const std::string& get_theme_of_work()const
+	{
+		return theme_of_work;
+	}
+	const std::string& get_date_of_diploma_defense()const
+	{
+		return date_of_diploma_defense;
+	}
+	int get_scores_of_practice()const
+	{
+		return scores_of_practice;
+	}
+	int get_scores_of_theory()const
+	{
+		return scores_of_theory;
+	}
+	void set_theme_of_work(const std::string& theme_of_work)
+	{
+		this->theme_of_work = theme_of_work;
+	}
+	void set_date_of_diploma_defense(const std::string& date_of_diploma_defense)
+	{
+		this->date_of_diploma_defense = date_of_diploma_defense;
+	}
+	void set_scores_of_practice(int scores_of_practice)
+	{
+		this->scores_of_practice = scores_of_practice;
+	}
+	void set_scores_of_theory(int scores_of_theory)
+	{
+		this->scores_of_theory = scores_of_theory;
+	}
+
+	//					constructors:
+	Graduate(HUMAN_TAKE_PARAMETERES, GRADUATE_TAKE_PARAMETERES) :Human(HUMAN_GIVE_PARAMETERES)
+	{
+		set_theme_of_work(theme_of_work);
+		set_date_of_diploma_defense(date_of_diploma_defense);
+		set_scores_of_practice(scores_of_practice);
+		set_scores_of_theory(scores_of_theory);
+		cout << "GConstructors:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	//				Methods:
+	void info()const
+	{
+		Human::info();
+		cout << "Тема дипломной работы - " << theme_of_work << ", " << "Дата защиты диплома - " << date_of_diploma_defense << ", " << "Баллы за практику - "<< scores_of_practice << ", " << "Баллы за теорию - " << scores_of_theory << endl;
+	}
 };
 
 void main()
@@ -182,4 +242,7 @@ void main()
 
 	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
 	teacher.info();
+
+	Graduate graduate("Danila", "Debilovi4", 25, "nifiga nedelanie", "20.09.2025", 80, 0);
+	graduate.info();
 }
