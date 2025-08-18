c3d4e03052a3fc76779c4cb3ceb33e422e2baf22
Danila <Danila@DESKTOP-44LP0PV>
2025-08-18 18:30:52 +0300

    .

--- Data/main.cpp
+++ Data/main.cpp
@@ -16,28 +16,28 @@ void main()
 		new Data(1980, 110, 15),
 		new Data(2005, 14, 123)
 	};
-	//Data data0(1975, 11, 44);
-	//Data data1(1976, 10, 12);
-	//Data data2(1976, 273, 124);
-	//data0.prinimaemaya_data(cout);
-	//data1.prinimaemaya_data(cout);
-	//data2.prinimaemaya_data(cout);
-	//Data data3(2, 8, 10);
-	//Data data4 = data0 + data3;
-	//data4.prinimaemaya_data(cout);
-	//Data data5(1024, 17, 11);
-	//data5.prinimaemaya_data(cout);
-	//Data data6 = data1 - data5;
-	//data6.prinimaemaya_data(cout);
+	Data data0(1975, 11, 44);
+	Data data1(1976, 10, 12);
+	Data data2(1976, 273, 124);
+	data0.prinimaemaya_data(cout);
+	data1.prinimaemaya_data(cout);
+	data2.prinimaemaya_data(cout);
+	Data data3(2, 8, 10);
+	Data data4 = data0 + data3;
+	data4.prinimaemaya_data(cout);
+	Data data5(1024, 17, 11);
+	data5.prinimaemaya_data(cout);
+	Data data6 = data1 - data5;
+	data6.prinimaemaya_data(cout);
 
 
 	std::ofstream fout("Data.txt");
-	//fout << data0 << endl;
-	//fout << data1 << endl;
-	//fout << data2 << endl;
-	//fout << data4 << endl;
-	//fout << data5 << endl;
-	//fout << data6 << endl;
+	fout << data0 << endl;
+	fout << data1 << endl;
+	fout << data2 << endl;
+	fout << data4 << endl;
+	fout << data5 << endl;
+	fout << data6 << endl;
 	for (int i = 0; i < sizeof(data_group) / sizeof(data_group[0]); i++)
 	{
 		data_group[i]->prinimaemaya_data(cout);
